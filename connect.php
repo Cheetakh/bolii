fedd0cc8c9cd8c98aefac7af55b81b8c65ff9c99
Cheetakh <dorjpalamhulan@gmail.com>
2025-04-01 21:13:25 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = getenv('aws.connect.psdb.cloud');
-$user = getenv('j9m9gma0v7235jowbmbo');
-$pass = getenv('pscale_pw_4gQU4PL1Yf1X58ZL9QdfibVnHBam7xAeE9wUkrKkdz1');
-$db = getenv('sanal');
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db = getenv('DB_NAME');
 
 $conn=new mysqli($host,$user,$pass,$db);
 
