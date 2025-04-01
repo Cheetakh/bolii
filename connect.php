5ff0f7fe0cae94ddbe1780051b2400bcc795b6b4
Cheetakh <dorjpalamhulan@gmail.com>
2025-04-01 21:58:38 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,11 @@
 <?php
 
 $host = getenv("DB_HOST");
-$user = getenv("DB_USERNAME");
-$pass = getenv("DB_PASSWORD");
 $db   = getenv("DB_NAME");
+$pass = getenv("DB_PASS");
+$user = getenv("DB_USER");
+
+
 
 // SSL сертификатын зам - GitHub-д нэмсэн `cacert.pem` файл
 $cert_path = __DIR__ . '/cacert.pem';
@@ -12,7 +14,7 @@ $mysqli = mysqli_init();
 $mysqli->ssl_set(NULL, NULL, $cert_path, NULL, NULL);
 
 // SSL client-ээр холбогдоно
-$mysqli->real_connect($host, $user, $pass, $db, 3306, NULL, MYSQLI_CLIENT_SSL);
+$mysqli->real_connect($host, $db, $pass, $user, 3306, NULL, MYSQLI_CLIENT_SSL);
 
 if ($mysqli->connect_errno) {
     echo "Холболтын алдаа гарлаа: " . $mysqli->connect_error;
