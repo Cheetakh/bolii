7735c75bab3d1b97ec1a951f53a73d0f96de81e9
Cheetakh <dorjpalamhulan@gmail.com>
2025-04-01 21:53:51 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = getenv("DB_HOST");
-$user = getenv("DB_NAME");
-$pass = getenv("DB_PASS");
-$db   = getenv("DB_USER");
+$user = getenv("DB_USERNAME");
+$pass = getenv("DB_PASSWORD");
+$db   = getenv("DB_NAME");
 
 // SSL сертификатын зам - GitHub-д нэмсэн `cacert.pem` файл
 $cert_path = __DIR__ . '/cacert.pem';
