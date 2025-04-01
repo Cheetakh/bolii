53b7d80180c3b3e9a45fd78995b8713be41827e6
Cheetakh <dorjpalamhulan@gmail.com>
2025-04-01 20:58:43 +0800

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,10 +1,12 @@
 <?php
 
-$host="aws.connect.psdb.cloud";
-$user="j9m9gma0v7235jowbmbo";
-$pass="pscale_pw_4gQU4PL1Yf1X58ZL9QdfibVnHBam7xAeE9wUkrKkdz1";
-$db="sanal";
+$host = getenv('aws.connect.psdb.cloud');
+$user = getenv('j9m9gma0v7235jowbmbo');
+$pass = getenv('pscale_pw_4gQU4PL1Yf1X58ZL9QdfibVnHBam7xAeE9wUkrKkdz1');
+$db = getenv('sanal');
+
 $conn=new mysqli($host,$user,$pass,$db);
+
 if($conn->connect_error){
     echo "Холболтын алдаа гарлаа.";
 }
